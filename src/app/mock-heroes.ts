3d5eb11853038c302dcbb8c32a7eef407a9acda0
Selven JOSEPH <joseph.selven@gmail.com>
2018-03-07 18:00:54 +0100

    Create displaying a List

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Superman' },
+  { id: 12, name: 'WonderWoman' },
+  { id: 13, name: 'Batman' },
+  { id: 14, name: 'Green Latern' },
+  { id: 15, name: 'Raven' },
+  { id: 16, name: 'Sinestro' },
+  { id: 17, name: 'Catwoman' },
+  { id: 18, name: 'Shazam' },
+  { id: 19, name: 'Flash' },
+  { id: 20, name: 'Nightwing' }
+];
